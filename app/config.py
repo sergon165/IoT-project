c0d74ff65814f72505f6ebc64397ae3f8e6f9c1b
Sergey Goncharov <sergon165@icloud.com>
2022-11-29 14:31:05 +0500

    edit config

--- app/config.py
+++ app/config.py
@@ -1,6 +1,6 @@
 class Config:
     def __init__(self):
-        self.INFLUX_HOST = 'influx'
+        self.INFLUX_HOST = 'localhost'
         self.INFLUX_PORT = 8086
         self.INFLUX_USER = 'admin'
         self.INFLUX_PASSWORD = 'admin123'
